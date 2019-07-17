8aa2bf616f2834ffd5041019088168542f0258e4
Melanie Harris <mell.j.harris@gmail.com>
2019-07-17 16:13:45 -0700

    still working

--- form-management/src/FormData.js
+++ form-management/src/FormData.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { withFormik, Form, Field } from "formik";
+import { withFormik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-function FormData() {
+function FormData(values, errors, touched) {
     return (
         <Form>
-
-            <Field type="text" name="name" placeholder="Insert Name Here" />
-            <Field type="email" name="email" placeholder=" Insert Email Here" />
-            <Field type="password" name="password" placeholder=" Insert Password Here" />
-            <Field type="checkbox" name="terms of service" />
-           
+            <div>
+                <Field type="text" name="name" placeholder="Insert Name Here" />
+            </div>
+            <div>
+            {touched.email &&  errors.email && <p> {errors.email}</p>}
+                <Field type="email" name="email" placeholder=" Insert Email Here" />
+            </div>
+            <div>
+                {touched.password &&  errors.password &&<p> {errors.password}</p>}
+                <Field type="password" name="password" placeholder=" Insert Password Here" />
+            </div>
+            <div>
+                <label>
+                <Field type="checkbox" name="termsOfService" checked={value.termsOfService} />
+                Accept terms of Service
+                </label>
+            </div>
             <button> Submit </button>
        </Form>
     ); 
@@ -17,15 +29,36 @@ function FormData() {
 
 const FormikFormData = withFormik({
 
-    mapPropsToValues({ username, password}) {
+    mapPropsToValues({ name, email, password, checkbox}) {
         return{
-              username: username || '',
-              password: password || ''  
+              name: name || '',
+              email: email || '',
+              password: password || '',
+            termsOfService: termsOfService || ''
+
         };
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string()
+            .name()
+            .required(),
+        email: Yup.string("Sorry, email is not valid")
+            .email("Sorry, email is required")
+            .required(),
+        password: Yup.string()
+            .min(6, "Password must be 6 or more characters")
+            .required(),
+        termsOfService: Yup.string()
+            .termsOfService("Checkbox is required to continue")
+            .required(),
+    }),
+    handleSubmit(values) {
+        console.log(values)
+        // form submission
     }
 }) (FormData);
 
 
 
-export default FormData;
+export default FormikFormData;
 
