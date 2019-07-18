10622d63c03a0b08e4a52e8818e05c83946534e4
Melanie Harris <mell.j.harris@gmail.com>
2019-07-17 20:05:56 -0700

    MVP met

--- form-management/src/FormData.js
+++ form-management/src/FormData.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import { withFormik, Form, Field, ErrorMessage } from "formik";
+import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
+import axios from "axios";
 
-function FormData(values, errors, touched) {
+function FormData({values, errors, touched}) {
     return (
         <Form>
             <div>
+                <br/>
+                Full Name: &nbsp;
                 <Field type="text" name="name" placeholder="Insert Name Here" />
             </div>
             <div>
+                Email: &nbsp;
             {touched.email &&  errors.email && <p> {errors.email}</p>}
                 <Field type="email" name="email" placeholder=" Insert Email Here" />
             </div>
             <div>
+                Password: &nbsp;
                 {touched.password &&  errors.password &&<p> {errors.password}</p>}
                 <Field type="password" name="password" placeholder=" Insert Password Here" />
             </div>
+            <br/>
             <div>
                 <label>
-                <Field type="checkbox" name="termsOfService" checked={value.termsOfService} />
-                Accept terms of Service
+                <Field type="checkbox" name="termsOfService" checked={values.termsOfService} />
+                Accept Terms Of Service
                 </label>
             </div>
             <button> Submit </button>
@@ -29,33 +35,45 @@ function FormData(values, errors, touched) {
 
 const FormikFormData = withFormik({
 
-    mapPropsToValues({ name, email, password, checkbox}) {
+    mapPropsToValues({ name, email, password, tos}) {
         return{
               name: name || '',
               email: email || '',
               password: password || '',
-            termsOfService: termsOfService || ''
+              tos: tos || false,
 
         };
     },
     validationSchema: Yup.object().shape({
-        name: Yup.string()
-            .name()
-            .required(),
-        email: Yup.string("Sorry, email is not valid")
-            .email("Sorry, email is required")
+       
+        email: Yup.string(" Email address is not valid")
+            .email("Email address is required")
             .required(),
         password: Yup.string()
-            .min(6, "Password must be 6 or more characters")
-            .required(),
-        termsOfService: Yup.string()
-            .termsOfService("Checkbox is required to continue")
+            .min(8, "Password must be 8 or more characters")
             .required(),
     }),
-    handleSubmit(values) {
-        console.log(values)
-        // form submission
+    handleSubmit(values, {resetForm, setErrors, setSubmitting}) {
+        if (values.email === "alreadytaken@atb.dev") {
+            setErrors({email: "That email is already taken"});
+        } else {
+            axios
+                .post("_https://reqres.in/api/users_", values)
+                console.log(values)
+                .then(res => {
+                    console.log(res); 
+                    resetForm();
+                    setSubmitting(false);
+                })
+                .catch(err => {
+                    console.log(err); 
+                    setSubmitting(false);
+                });
+        }
+        
     }
+
+    
 }) (FormData);
 
 
